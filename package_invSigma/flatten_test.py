5de5f034886fc0a03fb2b08a93fd0e07ea7be602
Lucas Makinen <lucas.makinen@gmail.com>
2024-10-28 12:42:06 +0000

    make tanh default activation

--- package_invSigma/flatten_test.py
+++ package_invSigma/flatten_test.py
@@ -115,7 +115,7 @@ model = custom_MLP([hidden_size,
                     n_params],
                   max_x = max_x, #jnp.array([MAX_MU, MAX_VAR]),
                   min_x = min_x, #jnp.array([MIN_MU, MIN_VAR]))
-                  act = smooth_leaky
+                  act = nn.tanh # new default activation
 )
 
 num = 10000
